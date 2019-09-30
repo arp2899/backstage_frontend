41bdc2301db743db65af1bd105947c843f7fc1b2
Arpit <inboxarpit990@gmail.com>
2019-09-30 16:31:17 +0100

    Setting up navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,22 +1,20 @@
 import Navbar from "react-bootstrap/lib/Navbar";
 import Nav from 'react-bootstrap/lib/Nav';
-import NavDropdown from 'react-bootstrap/lib/NavDropdown';
 import NavItem from 'react-bootstrap/lib/NavItem'
 import React from 'react';
 
-// import Button from 'react-bootstrap/Button';
 
 const NavBar = () => {
     return(
         <Navbar>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">BackStage</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <NavItem href="#home">Home</NavItem>
-                    <NavItem href="#home">Home</NavItem>
-                    <NavItem href="#home">Home</NavItem>
-                    <NavItem href="#home">Home</NavItem>
+                    <NavItem href="#home">About</NavItem>
+                    <NavItem href="#home">Product</NavItem>
+                    <NavItem href="#home">Contact</NavItem>
 
                 </Nav>
             </Navbar.Collapse>
